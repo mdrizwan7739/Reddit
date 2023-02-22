96cff30a062cd13049459dc165591e97d11f5889
Rizwan <mdrizwanansari7739@gmail.com>
2023-02-22 22:12:26 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
